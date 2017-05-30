ce06ee6b5d96ab11d4955008b34469b2db088e16
Jarno Rostedt <jarno.a.rostedt@student.samk.fi>
2017-05-30 21:05:52 +0300

    Icon added.

--- ArduinoSupercarUI/Form1.Designer.cs
+++ ArduinoSupercarUI/Form1.Designer.cs
@@ -410,6 +410,7 @@
             this.Controls.Add(this.UI_webcam_group);
             this.Controls.Add(this.UI_group_connection);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.Name = "arduinoUI";
